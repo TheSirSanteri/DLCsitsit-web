8190a62aaf5fbbd022c2c467fe36e0808e00753b
Santeri <Santeri.hilden@gmail.com>
2025-09-03 01:05:52 +0300

    Update to svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import deno from '@deno/svelte-adapter';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
   kit: {
-    // Single Page App -fallback index.html:iin
-    adapter: adapter({ fallback: 'index.html' })
+    adapter: deno()
   }
 };
-
 export default config;
