469b7524c900f07e2afbf20bcc8af8a70491bdab
Santeri <Santeri.hilden@gmail.com>
2025-09-04 21:34:25 +0300

    Trying to fix the Deploy problems

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@deno/svelte-adapter';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,8 @@ const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
-    prerender: { entries: ['*'] }
+    // Jos haluat edelleen prerenderöidä joitain reittejä:
+    // prerender: { entries: [] } // älä käytä ['*'] jos haluat SSR:n
   }
 };
 
