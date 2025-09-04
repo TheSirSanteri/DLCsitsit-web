3c3c0f0b13572c29d9dcadb0731658da4c86fdfb
Santeri <Santeri.hilden@gmail.com>
2025-09-04 17:58:26 +0300

    trying to deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
+  preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html'   // SPA-tyylinen fallback reititykselle
-    })
+    adapter: adapter(),
+    prerender: { entries: ['*'] }
   }
 };
-export default config;
 
+export default config;
