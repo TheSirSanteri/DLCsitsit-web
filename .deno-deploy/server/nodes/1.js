bc679a0d070058619bbe12a777a6f413325b0c8d
Santeri <Santeri.hilden@gmail.com>
2025-09-04 17:20:10 +0300

    removed .deno-deploy and trying to get npm n´build work

--- .deno-deploy/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.VtXA0ce5.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/UI-HHCHi.js","_app/immutable/chunks/9i0Um9ta.js","_app/immutable/chunks/C5Z_5Yfa.js"];
-export const stylesheets = [];
-export const fonts = [];
